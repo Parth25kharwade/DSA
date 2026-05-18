f89123c54d6cef673d4b8bc67818d6fc6aceb64a
Parth Kharwade <parthkharwade25@gmail.com>
2026-05-18 11:55:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (16.72%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    static int dp[];
+    public int climbStairs(int n) {
+        dp=new int[n+1];
+        return solve(n);
+    }
+    int solve(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        if(dp[n]!=0){
+            return dp[n];
+        }
+        int left=solve(n-1);
+        int right=solve(n-2);
+        dp[n]=left+right;
+        return dp[n];
+    }
+}
